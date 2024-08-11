35036acfdaf798ff186550477b1cafff9e4e7c95
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-11 21:38:26 +0300

    feat: add Utility.java

--- src/main/java/org/example/Utility.java
+++ src/main/java/org/example/Utility.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Utility {
+    public static void printTask(int taskNumber, String methodName) {
+        System.out.printf("Задача %d: %s%n", taskNumber, methodName);
+    }
+
+    public static void printResult() {
+        System.out.println("Результат работы задачи:\n");
+    }
+
+    public static int getIntInput(Scanner scanner, String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
+    public static String getStringInput(Scanner scanner, String message) {
+        System.out.print(message);
+        return scanner.nextLine();
+    }
 }
