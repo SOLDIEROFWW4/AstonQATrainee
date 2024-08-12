8770906145ba79559bcd25dfc11235c724fdf8b3
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-12 17:05:33 +0300

    refactor: refactor methods in Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,52 +16,55 @@ public class Main {
         Utility.printTask(3, "printColor");
         Tasks.printColor();
 
-        Utility.printTask(4, "isSumInRange");
+        Utility.printTask(4, "compareNumbers");
+        Tasks.compareNumbers();
+
+        Utility.printTask(5, "isSumInRange");
         int a = Utility.getIntInput(scanner, "Введите первое число: ");
         int b = Utility.getIntInput(scanner, "Введите второе число: ");
         System.out.println(Tasks.isSumInRange(a, b));
 
-        Utility.printTask(5, "printPositiveOrNegativeNumber");
+        Utility.printTask(6, "printPositiveOrNegativeNumber");
         int number = Utility.getIntInput(scanner, "Введите число: ");
         Tasks.printPositiveOrNegativeNumber(number);
 
-        Utility.printTask(6, "isNegativeNumber");
+        Utility.printTask(7, "isNegativeNumber");
         number = Utility.getIntInput(scanner, "Введите число: ");
         System.out.println(Tasks.isNegativeNumber(number));
 
-        Utility.printTask(7, "printStringMultipleTimes");
+        Utility.printTask(8, "printStringMultipleTimes");
         scanner.nextLine(); // consume newline
         String str = Utility.getStringInput(scanner, "Введите строку: ");
         int count = Utility.getIntInput(scanner, "Введите количество повторений: ");
         Tasks.printStringMultipleTimes(str, count);
 
-        Utility.printTask(8, "isLeapYear");
+        Utility.printTask(9, "isLeapYear");
         int year = Utility.getIntInput(scanner, "Введите год: ");
         System.out.println(Tasks.isLeapYear(year));
 
-        Utility.printTask(9, "invertBinaryArray");
+        Utility.printTask(10, "invertBinaryArray");
         int size = Utility.getIntInput(scanner, "Введите размер массива: ");
         int[] binaryArray = Tasks.createBinaryArray(size);
         Tasks.invertBinaryArray(binaryArray);
         Utility.printResult();
         Tasks.printArray(binaryArray);
 
-        Utility.printTask(10, "fillArray");
+        Utility.printTask(11, "fillArray");
         int[] fillArray = Tasks.fillArray();
         Utility.printResult();
         Tasks.printArray(fillArray);
 
-        Utility.printTask(11, "processArray");
+        Utility.printTask(12, "processArray");
         Utility.printResult();
         Tasks.printArray(Tasks.processArray());
 
-        Utility.printTask(12, "fillDiagonals");
+        Utility.printTask(13, "fillDiagonals");
         int size2DArray = Utility.getIntInput(scanner, "Введите размер двумерного массива: ");
         int[][] array = new int[size2DArray][size2DArray];
         Utility.printResult();
         Tasks.print2DArray(array);
 
-        Utility.printTask(13, "initializeArray");
+        Utility.printTask(14, "initializeArray");
         int len = Utility.getIntInput(scanner, "Введите длину массива: ");
         int initialValue = Utility.getIntInput(scanner, "Введите начальное значение: ");
         Utility.printResult();
