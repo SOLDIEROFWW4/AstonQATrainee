675327532a47507ab3f6c14571738abdefd0eda1
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:26:47 +0300

    feat: update Dog.java

--- src/main/java/ru/astondev/lesson7/Dog.java
+++ src/main/java/ru/astondev/lesson7/Dog.java
@@ -1,4 +1,30 @@
 package ru.astondev.lesson7;
 
-public class Dog {
+public class Dog extends Animal{
+    static int totalDogs = 0;
+    private static final int MAX_RUN_DISTANCE = 500;
+    private static final int MAX_SWIM_DISTANCE = 10;
+
+    public Dog(String name) {
+        super(name);
+        totalDogs++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= MAX_RUN_DISTANCE) {
+            System.out.println(getName() + " пробежал " + distance + " м.");
+        } else {
+            System.out.println(getName() + " не может пробежать " + distance + " м.");
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance <= MAX_SWIM_DISTANCE) {
+            System.out.println(getName() + " проплыл " + distance + " м.");
+        } else {
+            System.out.println(getName() + " не может пробежать " + distance + " м.");
+        }
+    }
 }
