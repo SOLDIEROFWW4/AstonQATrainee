bb9d2a8400dde726268df2915833c9683b7f2590
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:49:06 +0300

    feat: add Shape interface

---
+++ src/main/java/ru/astondev/lesson7/Shape.java
@@ -0,0 +1,9 @@
+package ru.astondev.lesson7;
+
+interface Shape {
+    double getPerimeter();
+    double getArea();
+    String getFillColor();
+    String getBorderColor();
+    void printDetails();
+}
