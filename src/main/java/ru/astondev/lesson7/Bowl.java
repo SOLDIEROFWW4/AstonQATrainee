8e0ede7e64be208594501fd468503c2d77319766
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:26:59 +0300

    feat: create and update Bowl.java

---
+++ src/main/java/ru/astondev/lesson7/Bowl.java
@@ -0,0 +1,25 @@
+package ru.astondev.lesson7;
+
+public class Bowl {
+    private int food;
+
+    public Bowl(int food) {
+        this.food = Math.max(food, 0);
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void addFood(int amount) {
+        if (amount > 0) {
+            food += amount;
+        }
+    }
+
+    public void decreaseFood(int amount) {
+        if (food >= amount) {
+            food -= amount;
+        }
+    }
+}
