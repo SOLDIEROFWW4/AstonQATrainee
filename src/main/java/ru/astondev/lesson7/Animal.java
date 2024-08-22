d70bfb87193f61740d52f5378f7387b008aabfc6
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:26:31 +0300

    feat: update Animal.java

--- src/main/java/ru/astondev/lesson7/Animal.java
+++ src/main/java/ru/astondev/lesson7/Animal.java
@@ -1,4 +1,19 @@
 package ru.astondev.lesson7;
 
 public abstract class Animal {
+    static int totalAnimals = 0;
+    protected String name;
+
+    public Animal(String name) {
+        this.name = name;
+        totalAnimals++;
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+
+    public String getName() {
+        return name;
+    }
 }
