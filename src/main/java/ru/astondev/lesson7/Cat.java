6ef1de69cf9f10afcb5eca1a1e2338ed84e81a07
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:26:41 +0300

    feat: update Cat.java

--- src/main/java/ru/astondev/lesson7/Cat.java
+++ src/main/java/ru/astondev/lesson7/Cat.java
@@ -1,4 +1,29 @@
 package ru.astondev.lesson7;
 
-public class Cat {
+public class Cat extends Animal{
+    static int totalCats = 0;
+    private static final int MAX_RUN_DISTANCE = 200;
+    private boolean satiety;
+
+
+    public Cat(String name) {
+        super(name);
+        this.satiety = false;
+        totalCats++;
+    }
+
+    @Override
+    public void run(int distance) {
+        if (distance <= MAX_RUN_DISTANCE) {
+            System.out.println(getName() + " пробежал " + distance + " м.");
+        } else {
+            System.out.println(getName() + " не может пробежать " + distance + " м.");
+        }
+
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println(getName() + " не умеет плавать.");
+    }
 }
