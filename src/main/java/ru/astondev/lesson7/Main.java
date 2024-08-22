193352317cca03dc116c92eb7957a40e4cb31f1c
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-22 22:35:48 +0300

    feat: update Main.java

--- src/main/java/ru/astondev/lesson7/Main.java
+++ src/main/java/ru/astondev/lesson7/Main.java
@@ -2,6 +2,18 @@ package ru.astondev.lesson7;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Cat[] cats = { new Cat("Снежок"), new Cat("Барсик"), new Cat("Марс")};
+
+        Bowl bowl = new Bowl(20);
+
+        for (Cat cat : cats) {
+            cat.eat(bowl, 10);
+        }
+
+        for (Cat cat : cats) {
+            System.out.println(cat.getName() + " сыт: " + cat.isSatiety());
+        }
+
+        bowl.addFood(30);
     }
 }
