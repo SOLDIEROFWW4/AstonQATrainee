5eacc43337e0c9b1bf765c6651bb8e20ce082eea
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-23 14:36:23 +0300

    feat: update Rectangle.java logic

--- src/main/java/ru/astondev/lesson7/Rectangle.java
+++ src/main/java/ru/astondev/lesson7/Rectangle.java
@@ -1,4 +1,22 @@
 package ru.astondev.lesson7;
 
-public class Rectangle {
+public class Rectangle extends BaseShape{
+    private double width;
+    private double height;
+
+    public Rectangle(String fillColor, String borderColor, double width, double height) {
+        super(fillColor, borderColor);
+        this.height = height;
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 + (width * height);
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
 }
