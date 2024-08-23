3feec80e1a4e232b926072d163f199ca48a613b4
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-23 14:42:44 +0300

    feat: update Triangle.java logic

--- src/main/java/ru/astondev/lesson7/Triangle.java
+++ src/main/java/ru/astondev/lesson7/Triangle.java
@@ -1,4 +1,25 @@
 package ru.astondev.lesson7;
 
-public class Triangle {
+public class Triangle extends BaseShape{
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(String fillColor, String borderColor, double sideA, double sideB, double sideC) {
+        super(fillColor, borderColor);
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+
+    @Override
+    public double getArea() {
+        double s = getPerimeter() / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s-sideC));
+    }
 }
