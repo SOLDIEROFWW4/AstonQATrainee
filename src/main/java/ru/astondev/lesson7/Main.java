2b284f8fdf7457d281c160124be208f468eb83b0
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-24 14:07:49 +0300

    docs: update README.md

--- src/main/java/ru/astondev/lesson7/Main.java
+++ src/main/java/ru/astondev/lesson7/Main.java
@@ -1,8 +1,15 @@
 package ru.astondev.lesson7;
 
+import ru.astondev.lesson7.task1.Bowl;
+import ru.astondev.lesson7.task1.Cat;
+import ru.astondev.lesson7.task2.Circle;
+import ru.astondev.lesson7.task2.Rectangle;
+import ru.astondev.lesson7.task2.Shape;
+import ru.astondev.lesson7.task2.Triangle;
+
 public class Main {
     public static void main(String[] args) {
-        Cat[] cats = { new Cat("Снежок"), new Cat("Барсик"), new Cat("Марс")};
+       Cat[] cats = { new Cat("Снежок"), new Cat("Барсик"), new Cat("Марс")};
 
         Bowl bowl = new Bowl(20);
 
@@ -15,5 +22,13 @@ public class Main {
         }
 
         bowl.addFood(30);
+
+        /*Shape circle = new Circle("Black", "Red", 5);
+        Shape rectangle = new Rectangle("Blue", "Yellow",4 ,6 );
+        Shape triangle = new Triangle("Green", "Purple", 5, 4,6 );
+
+        circle.printDetails();
+        rectangle.printDetails();
+        triangle.printDetails();*/
     }
 }
