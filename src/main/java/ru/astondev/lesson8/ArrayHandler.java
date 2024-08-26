58a7063d8824b1bbabd1069264758232c673e9eb
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-26 22:33:56 +0300

    feat: update ArrayHandler

--- src/main/java/ru/astondev/lesson8/ArrayHandler.java
+++ src/main/java/ru/astondev/lesson8/ArrayHandler.java
@@ -1,4 +1,6 @@
 package ru.astondev.lesson8;
 
 public abstract class ArrayHandler {
+    protected abstract void validateArraySize(String[][] array) throws MyArraySizeException;
+    protected abstract int parseElement(String element, int row, int col) throws MyArrayDataException;
 }
