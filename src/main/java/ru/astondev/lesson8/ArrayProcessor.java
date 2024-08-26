4309942a7b62dcae63b61a1d1b40d62ca5314f92
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-26 22:44:40 +0300

    feat: update ArrayProcessor

--- src/main/java/ru/astondev/lesson8/ArrayProcessor.java
+++ src/main/java/ru/astondev/lesson8/ArrayProcessor.java
@@ -1,4 +1,21 @@
 package ru.astondev.lesson8;
 
-public class ArrayProcessor {
+public class ArrayProcessor extends ArrayHandler{
+    private static final int ARRAY_SIZE = 4;
+
+    @Override
+    protected void validateArraySize(String[][] array) throws MyArraySizeException {
+        if (array.length != ARRAY_SIZE || array[0].length != ARRAY_SIZE) {
+            throw new MyArraySizeException("Массив должен иметь размер 4 на 4");
+        }
+    }
+
+    @Override
+    protected int parseElement(String element, int row, int col) throws MyArrayDataException {
+        try {
+            return Integer.parseInt(element);
+        } catch (NumberFormatException e){
+            throw new MyArrayDataException("Неверные данные в (строке: " + row + ", столбце:" + col + "): " + element);
+        }
+    }
 }
