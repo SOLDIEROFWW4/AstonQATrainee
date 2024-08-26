cca02ab620d1c01c1a21f749ae0d0b3504ed91f4
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-08-26 22:16:19 +0300

    feat: update MyArraySizeException

--- src/main/java/ru/astondev/lesson8/MyArraySizeException.java
+++ src/main/java/ru/astondev/lesson8/MyArraySizeException.java
@@ -1,4 +1,7 @@
 package ru.astondev.lesson8;
 
-public class MyArraySizeException {
+public class MyArraySizeException extends Exception{
+    public MyArraySizeException(String message) {
+        super(message);
+    }
 }
