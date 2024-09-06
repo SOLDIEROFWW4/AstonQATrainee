2240c47222267343efcea853c97e4cb0d81d0e74
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-06 14:19:59 +0300

    feat: update WordProcessor.java, fix some problems

--- src/main/java/ru/astondev/lesson11/Main.java
+++ src/main/java/ru/astondev/lesson11/Main.java
@@ -2,6 +2,6 @@ package ru.astondev.lesson11;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
--- src/main/java/ru/astondev/lesson11/WordProcessor.java
+++ src/main/java/ru/astondev/lesson11/WordProcessor.java
@@ -1,4 +1,24 @@
 package ru.astondev.lesson11;
 
+import java.awt.*;
+import java.util.*;
+
 public class WordProcessor {
+    private String[] words;
+
+    public WordProcessor(String[] words) {
+        this.words = words;
+    }
+
+    public Set<String> getUniqueWords() {
+        return new HashSet<>(Arrays.asList(words));
+    }
+
+    public Map<String, Integer> getWordFrequencies() {
+        Map<String,Integer> wordCount = new HashMap<>();
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+        return wordCount;
+    }
 }
