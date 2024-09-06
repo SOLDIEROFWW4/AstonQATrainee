222f3b67bb46df13fcdf1235c2c632e915f555d8
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-06 14:51:08 +0300

    feat: update PhoneBook.java

--- src/main/java/ru/astondev/lesson11/task2/PhoneBook.java
+++ src/main/java/ru/astondev/lesson11/task2/PhoneBook.java
@@ -1,4 +1,19 @@
 package ru.astondev.lesson11.task2;
 
+import java.util.*;
+
 public class PhoneBook {
+    private Map<String, List<String>> contacts;
+
+    private PhoneBook() {
+        contacts = new HashMap<>();
+    }
+
+    public void add(String lastName, String phoneNumber) {
+        contacts.computeIfAbsent(lastName, k-> new ArrayList<>()).add(phoneNumber);
+    }
+
+    public List<String> get(String lastName) {
+        return contacts.getOrDefault(lastName, Collections.emptyList());
+    }
 }
