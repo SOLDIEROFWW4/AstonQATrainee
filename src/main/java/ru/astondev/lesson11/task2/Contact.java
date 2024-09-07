102f40828d9e81489335234de783b080cd52c170
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-07 16:47:03 +0300

    feat: add comments in Contact.java

--- src/main/java/ru/astondev/lesson11/task2/Contact.java
+++ src/main/java/ru/astondev/lesson11/task2/Contact.java
@@ -3,23 +3,34 @@ package ru.astondev.lesson11.task2;
 import java.util.*;
 
 public class Contact {
+    // Переменная lastName типа String для
     private String lastName;
+    // List для хранения телефонных номеров контакта
     private List<String> phoneNumbers;
 
+    // Конструктор класса для инициализации значений
     public Contact(String lastName) {
+        // Инициализируем поле lastName переданной фамилией
         this.lastName = lastName;
+        // Инициализируем пустой List для хранения телефонных номеров
         this.phoneNumbers = new ArrayList<>();
     }
 
+    // Метод для добавления телефонного номера
     public void addPhoneNumber(String phoneNumber) {
+        // Добавляем переданный номер в список phoneNumbers
         phoneNumbers.add(phoneNumber);
     }
 
+    // Метод для получения всех телефонных номеров
     public List<String> getPhoneNumbers() {
+        // Возвращаем список телефонных номеров
         return phoneNumbers;
     }
 
+    // Метод для получения фамилии контакта
     public String getLastName() {
+        // Возвращаем фамилию
         return lastName;
     }
 }
