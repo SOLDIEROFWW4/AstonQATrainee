774656880cff817e2803fd5a95bdea972413e071
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-10 10:16:11 +0300

    feat: update Main.java

--- src/main/java/ru/astondev/lesson12_TestNG/Main.java
+++ src/main/java/ru/astondev/lesson12_TestNG/Main.java
@@ -2,6 +2,9 @@ package ru.astondev.lesson12_TestNG;
 
 public class Main {
     public static void main(String[] args) {
-        
+        FactorialCalculator calculator = new RecursiveFactorialCalculator();
+        int number = 5;
+        long result = calculator.calculate(number);
+        System.out.println("Факториал " + number + " = " + result);
     }
 }
