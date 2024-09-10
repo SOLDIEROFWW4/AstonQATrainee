134c80854df070c2d09058b815926272a8270281
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-10 10:36:53 +0300

    feat: update FactrorialCalculatorTest with TestNG

--- src/test/java/lesson12_TestNG/FactorialCalculatorTest.java
+++ src/test/java/lesson12_TestNG/FactorialCalculatorTest.java
@@ -1,5 +1,46 @@
 package lesson12_TestNG;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.astondev.lesson12_TestNG.FactorialCalculator;
+import ru.astondev.lesson12_TestNG.RecursiveFactorialCalculator;
+
 public class FactorialCalculatorTest {
-    
+
+    private FactorialCalculator calculator;
+
+    @BeforeMethod
+    public void setUp() {
+        calculator = new RecursiveFactorialCalculator();
+    }
+
+    @Test
+    public void testFactorialOfZero() {
+        long result = calculator.calculate(0);
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testFactorialOfOne() {
+        long result = calculator.calculate(1);
+        Assert.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testFactorialOfFive() {
+        long result = calculator.calculate(5);
+        Assert.assertEquals(result, 120);
+    }
+
+    @Test
+    public void testFactorialOfTen() {
+        long result = calculator.calculate(10);
+        Assert.assertEquals(result, 3628800);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFactorialOfNegativeNumber() {
+        calculator.calculate(-1);
+    }
 }
