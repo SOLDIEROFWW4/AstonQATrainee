d307f0493fc50691a14d8033e08ab97b45fea01f
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-10 10:11:35 +0300

    feat: update RecursiveFactorialCalculator

--- src/main/java/ru/astondev/lesson12_TestNG/RecursiveFactorialCalculator.java
+++ src/main/java/ru/astondev/lesson12_TestNG/RecursiveFactorialCalculator.java
@@ -1,4 +1,11 @@
 package ru.astondev.lesson12_TestNG;
 
-public class RecursiveFactorialCalculator {
+public class RecursiveFactorialCalculator implements FactorialCalculator{
+    @Override
+    public long calculate(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Число должно быть положительным");
+        }
+        return (n<=1) ? 1 : n* calculate(n-1);
+    }
 }
