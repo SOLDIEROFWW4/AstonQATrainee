0a6f6b10e8df6ef87b3dc6e078dc4ff54137df4f
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-19 13:54:16 +0300

    feat: update TestHelper logic

--- src/main/java/ru/astondev/lesson15/TestHelper.java
+++ src/main/java/ru/astondev/lesson15/TestHelper.java
@@ -1,4 +1,17 @@
 package ru.astondev.lesson15;
 
+import org.openqa.selenium.WebElement;
+
 public class TestHelper {
+    public static void assertTextEquals(String expected, String actual, String errorMessage) {
+        if (!expected.equals(actual)) {
+            throw new AssertionError(errorMessage + ": ожидалось [" + expected + " ], но найдено [" + actual + " ]");
+        }
+    }
+
+    public static void assertElementDisplayed(WebElement element, String errorMessage) {
+        if (!element.isDisplayed()) {
+            throw new AssertionError(errorMessage);
+        }
+    }
 }
