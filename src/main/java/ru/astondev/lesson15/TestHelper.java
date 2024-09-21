c9f1c04d9d5ed9a03c9154bc9082406fe6e09416
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-21 17:42:37 +0300

    refactor: refactor code structure

--- src/main/java/ru/astondev/lesson15/TestHelper.java
+++ src/main/java/ru/astondev/lesson15/TestHelper.java
@@ -6,8 +6,7 @@ public class TestHelper {
     public static void assertTextEquals(String expected, String actual, String errorMessage) {
         if (!expected.equals(actual)) {
             throw new AssertionError(errorMessage + ": ожидалось [" + expected + " ], но найдено [" + actual + " ]");
-        }
-        else {
+        } else {
             System.out.println("Тест пройден успешно. Текст соответствует ожидаемому результату.");
         }
     }
@@ -15,8 +14,7 @@ public class TestHelper {
     public static void assertElementDisplayed(WebElement element, String errorMessage) {
         if (!element.isDisplayed()) {
             throw new AssertionError(errorMessage);
-        }
-        else {
+        } else {
             System.out.println("Тест пройден успешно. Элемент отображается.");
         }
     }
