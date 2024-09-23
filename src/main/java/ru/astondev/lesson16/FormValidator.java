3ce293b16c59adb9abc052e50d0a63fee52a3135
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-23 19:37:45 +0300

    feat: update FormValidator

--- src/main/java/ru/astondev/lesson16/FormValidator.java
+++ src/main/java/ru/astondev/lesson16/FormValidator.java
@@ -1,4 +1,15 @@
 package ru.astondev.lesson16;
 
+import org.openqa.selenium.WebDriver;
+
 public class FormValidator {
+    private WebDriver webDriver;
+
+    public FormValidator(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void validateFormFields() {
+        
+    }
 }
