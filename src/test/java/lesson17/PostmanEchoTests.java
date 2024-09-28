b4f8bea75247119c66f991bf49b4ac580af489ae
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2024-09-28 19:16:12 +0300

    feat: update PostmanEchoTests.java

--- src/test/java/lesson17/PostmanEchoTests.java
+++ src/test/java/lesson17/PostmanEchoTests.java
@@ -2,14 +2,19 @@ package lesson17;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class PostmanEchoTests {
+
+    private final static String postmanEchoPath = "https://postman-echo.com";
+
     @Test
     public void givenArgs_whenGetArgs_thenStatus200() {
-        RestAssured.baseURI = "https://postman-echo.com";
+        RestAssured.baseURI = postmanEchoPath;
 
         String orderReq = "{\n" +
                 "        \"foo1\": \"bar1\",\n" +
@@ -22,8 +27,120 @@ public class PostmanEchoTests {
                 .contentType(ContentType.JSON)
                 .body(orderReq)
                 .get("/get")
-                .then()
-                    .statusCode(200)
-                    .log().all();
+        .then()
+                .statusCode(200)
+                .log().all();
+
+    }
+
+    @Test
+    public void whenRawTextCreated_thenStatus200() {
+        RestAssured.baseURI = postmanEchoPath;
+
+        String rawText = "{\n " +
+                "\"test\": \"value\"\n" +
+                "}";
+
+        given()
+                .contentType("text/plain")
+                .body(rawText)
+                .log().all()
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .log().all()
+                .body("data", equalTo(rawText));
+    }
+
+    @Test
+    public void whenPostFormData_thenStatus200() {
+        RestAssured.baseURI = postmanEchoPath;
+
+        given()
+                .contentType("application/json")
+                .body("{" +
+                        "\"foo1\":\"bar1\", " +
+                        "\"foo2\":\"bar2\"" +
+                        "}")
+                .log().all()
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200) 
+                .log().all()
+                .body("data.foo1", equalTo("bar1"))
+                .body("data.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void whenUpdateText_thenStatus200() {
+        RestAssured.baseURI = postmanEchoPath;
+
+        String requestBody = "This is expected to be sent back as part of response body.";
+
+        Response response =
+        given()
+                .queryParam("hand", "wave")
+                .header("Content-Type", "text/plain")
+                .body(requestBody)
+        .when()
+                .put("/put")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo(requestBody))
+                .extract()
+                .response();
+
+        System.out.println("Response Body:");
+        System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public void whenSemiUpdateText_thenStatus200() {
+        RestAssured.baseURI = postmanEchoPath;
+
+        String requestBody = "This is expected to be sent back as part of response body.";
+
+        Response response =
+        given()
+                .contentType("text/plain")
+                .body(requestBody)
+                .log().all()
+        .when()
+                .patch("/patch")
+        .then()
+                .statusCode(200)
+                .log().all()
+                .body("data", equalTo(requestBody))
+                .extract()
+                .response();
+
+        System.out.println("Response Body:");
+        System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public void givenRawText_whenDeleteText_thenStatus200() {
+        RestAssured.baseURI = postmanEchoPath;
+
+        String requestBody = "This is expected to be sent back as part of response body.";
+
+        Response response =
+        given()
+                .contentType("text/plain")
+                .body(requestBody)
+                .log().all()
+        .when()
+                .delete("/delete")
+        .then()
+                .statusCode(200)
+                .log().all()
+                .body("data", equalTo(requestBody))
+                .extract()
+                .response();
+
+        System.out.println("Response Body:");
+        System.out.println(response.getBody().asString());
     }
 }
